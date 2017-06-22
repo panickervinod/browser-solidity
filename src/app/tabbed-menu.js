77bcf7ac4c6678c58014bec1a246c38df0081b62
ninabreznik <ninabreznik@gmail.com>
2017-06-22 16:09:31 +0200

    Rename .envView/#envView into .compileView/#compileTabView

--- src/app/tabbed-menu.js
+++ src/app/tabbed-menu.js
@@ -13,7 +13,7 @@ function tabbedMenu (container, appAPI, events, opts) {
   })
 
   // initialize tabbed menu
-  selectTab(container.querySelector('.envView'))
+  selectTab(container.querySelector('.compileView'))
 
   // add event listeners for loading spinner
   events.compiler.register('loadingCompiler', function start () {
